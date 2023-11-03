ad477a79fc1aaddf7052155d18f38f6d92710076
Salomon Nghukam <dsalomonj@gmail.com>
2023-11-03 19:03:27 +0100

    Added test case

--- test.js
+++ test.js
@@ -1,6 +1,26 @@
 const { formatPhoneNumber, getMobileOperator, getPhoneNumberInfo, Errors } = require("./index");
 
 try {
+	const phoneNumber = "17645685126";
+	const code = "DE";
+	const formattedNumber = formatPhoneNumber(phoneNumber, code);
+
+	const operator = getMobileOperator(phoneNumber, code);
+
+	const phoneNumberInfo = getPhoneNumberInfo(phoneNumber, code);
+
+	console.log(`Original Number: ${phoneNumber}`);
+	console.log(`Formatted Number: ${formattedNumber}`);
+	console.log(`Mobile Operator: ${operator}`);
+	console.log(`Phone Number Info: ${JSON.stringify(phoneNumberInfo)}`);
+	console.assert(phoneNumberInfo.formattedNumber == formattedNumber);
+	console.assert(phoneNumberInfo.countryCode == code);
+	console.assert(phoneNumberInfo.operator == operator);
+} catch (error) {
+	console.log(error.message);
+}
+
+try {
 	const phoneNumber = "11645685126";
 	const code = "DE";
 	const formattedNumber = formatPhoneNumber(phoneNumber, code);
